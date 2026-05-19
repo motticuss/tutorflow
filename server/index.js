9ecc9b4e506f7fc6b504173542a5860046ae4cde
motticuss <motticuss@gmail.com>
2026-05-18 20:46:23 -0400

    Fix CORS to allow Vercel frontend

--- server/index.js
+++ server/index.js
@@ -13,7 +13,12 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://tutorflow-one.vercel.app',
+    process.env.CLIENT_URL,
+  ],
   credentials: true,
 }));
 app.use(express.json());
